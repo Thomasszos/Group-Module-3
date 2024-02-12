2af31ecee60fafe636752dad251dc4fc2247b988
Yaghs <syaghinloo@gmail.com>
2024-02-12 14:37:57 -0500

    test

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -2,17 +2,22 @@ package com.mycompany.agency;
 
 
 
-    // TODO 1: Make Intern a child of StaffEmploee
+// TODO 1: Make Intern a child of StaffEmploee
 
-public class Intern 
+public class Intern extends StaffEmployee
 {
+
+
+
+
     //-----------------------------------------------------------------
     //  TODO 2: Constructor: Sets up this intern using the specified
     //  information.
     //-----------------------------------------------------------------
-    public Intern(String eName, String eAddress, String ePhone)
-    {
-        
+
+
+    public Intern(String eName, String eAddress, String ePhone, String socSecNumber, double rate) {
+        super(eName, eAddress, ePhone, socSecNumber, rate);
     }
 
     //-----------------------------------------------------------------
@@ -20,6 +25,6 @@ public class Intern
     //-----------------------------------------------------------------
     public double pay()
     {
-      
+        return 0;
     }
 }
